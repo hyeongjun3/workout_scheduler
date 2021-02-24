e0edeb1c4fc220bc4f6004b21aaacf6045e63f78
hyeongjun94 <whow1101@naver.com>
2021-02-24 23:09:32 +0900

    Title : [New Feature][UI] WS-30
    Decription :
    later create default.css file which contain serveral variables e.g) --font-smal-size
    
    Estimated MD : 1H
    
    Real MD : 48M

--- workout-stie/scripts/private.js
+++ workout-stie/scripts/private.js
@@ -1,4 +1,21 @@
 import myHeader from './header.js'
+import MyRequest from './request'
 
 let my_header = new myHeader(document);
 my_header.setUI();
+
+let my_request = new MyRequest();
+
+const update_btn_elem = document.querySelector('button.update');
+const withdrawl_btn_elem = document.querySelector('button.withdrawl');
+
+const nickname_input_elem = document.querySelector('input#nickname');
+const male_input_elem = document.querySelector('input#male');
+const female_input_elem = document.querySelector('input#female');
+
+window.addEventListener('DOMContentLoaded', e => {
+    let input = {'access_token' : window.sessionStorage.getItem('access_token')};
+    my_request.getUserInfo(input)
+    .then(result => {
+        ]})
+})
