1119eea3c1245a9c7da7bcc82af178327df62d44
hyeongjun94 <whow1101@naver.com>
2021-04-17 21:00:04 +0900

    Title : [Feature][User] 로그인 컴포넌트
    Jira Number : WS-114
    
    Description :
    MVC 패턴이 익숙하지 못하고 가독성이 많이 떨어진다 수정이 필요해보인다.
    
    Unit test :
    로그인 UI 컴포넌트
    -> O
    로그인 유효하지 않을 시 알려줄 다이얼로그 UI 컴포넌트
    -> O
    이메일, 패스워드 포맷이 유효하지 않을 시 유저에게 알려줄 알림 및 로그인 버튼 비활성화
    -> O
    로그인 백엔드 통신
    -> X, 백엔드 구현 X
    로그인 완료 시 daily 페이지로 라우팅
    -> O
    소셜 로그인 통신
    -> X, 백엔드 구현 X
    token 관리
    -> X, 백엔드 구현 X1234

---
+++ workout-stie/frontend/scripts/user/signModel.js
@@ -0,0 +1,184 @@
+let myRequest = null;
+
+/* Checking window object if it has my request module */
+if (window.hasOwnProperty('myRequest') === false) {
+  import('../request.js').then((module) => {
+    myRequest = module.MyRequest;
+  });
+}
+
+function UserInputFieldForm(type, nameKor, nameEng, inputId, infoStr) {
+  this.type = type;
+  this.nameKor = nameKor;
+  this.nameEng = nameEng;
+  this.inputId = inputId;
+  this.infoStr = infoStr;
+  this.value = null;
+}
+
+function SignModel() {
+  this.email = null;
+  this.pwd = null;
+}
+
+SignModel.prototype.bindSetEmailAlert = function (callback) {
+  this.setEmailAlert = callback;
+};
+
+SignModel.prototype.bindSetBtn = function (callback) {
+  this.setBtn = callback;
+};
+
+SignModel.prototype.bindSetModal = function (callback) {
+  this.setModal = callback;
+};
+
+SignModel.prototype.bindShowModal = function (callback) {
+  this.showModal = callback;
+};
+
+SignModel.prototype.setValue = function (index, value) {
+  this.inputFieldForm[index].value = value;
+};
+
+SignModel.prototype.checkValidForm = function () {
+  if (this.emailFlag === true && this.pwdFlag === true) {
+    this.setBtn(true);
+  } else {
+    this.setBtn(false);
+  }
+};
+
+SignModel.prototype.setEmail = function (emailInput) {
+  this.setValue(0, emailInput);
+
+  if (this.validateEmail(emailInput)) {
+    this.setEmailAlert(false);
+    this.emailFlag = true;
+  } else {
+    this.setEmailAlert(true);
+    this.emailFlag = false;
+  }
+
+  this.checkValidForm();
+};
+
+SignModel.prototype.setPwd = function (pwdInput) {
+  this.setValue(1, pwdInput);
+
+  this.pwdFlag = true;
+
+  this.checkValidForm();
+};
+
+SignModel.prototype.validateEmail = function (email) {
+  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+};
+
+function SignInModel() {
+  SignModel.call(this);
+  this.inputFieldForm = [
+    new UserInputFieldForm(
+      'email',
+      '이메일',
+      'email',
+      'input-email',
+      '잘못된 형식입니다.'
+    ),
+    new UserInputFieldForm('password', '비밀번호', 'pwd', 'input-pwd', ''),
+  ];
+}
+
+SignInModel.prototype = Object.create(SignModel.prototype);
+
+/* Call back functions that will be invoked by View */
+SignInModel.prototype.signIn = function () {
+  this.email = this.inputFieldForm[0].value;
+  this.pwd = this.inputFieldForm[1].value;
+
+  myRequest
+    .signUp(this.email, this.pwd)
+    .then((result) => {
+      /* TODO : should be implement after rest api is implemented */
+    })
+    .catch((err) => {
+      console.error(err);
+      this.setModal('에러발생', err.msg);
+      this.showModal();
+    });
+};
+
+function SignUpModel() {
+  SignModel.call(this);
+  this.emailFlag = false;
+  this.pwdFlag = false;
+  /* Use array because of order */
+  this.inputFieldForm = [
+    new UserInputFieldForm(
+      'email',
+      '이메일',
+      'email',
+      'input-email',
+      '잘못된 형식입니다'
+    ),
+    new UserInputFieldForm('password', '비밀번호', 'pwd', 'input-pwd', ''),
+    new UserInputFieldForm(
+      'password',
+      '비밀번호 확인',
+      'pwd2',
+      'input-pwd2',
+      '비밀번호가 일치하지 않습니다'
+    ),
+  ];
+}
+
+SignUpModel.prototype = Object.create(SignModel.prototype);
+
+SignUpModel.prototype.bindSetPwdAlert = function (callback) {
+  this.setPwdAlert = callback;
+};
+
+SignUpModel.prototype.bindHideModal = function (callback) {
+  this.hideModal = callback;
+};
+
+SignUpModel.prototype.checkValidForm = function () {
+  if (this.emailFlag === true && this.pwdFlag === true) {
+    this.setBtn(true);
+  } else {
+    this.setBtn(false);
+  }
+};
+
+SignUpModel.prototype.setPwd2 = function (pwd2Input) {
+  this.setValue(2, pwd2Input);
+
+  if (this.inputFieldForm[1].value === this.inputFieldForm[2].value) {
+    this.setPwdAlert(false);
+    this.pwdFlag = true;
+  } else {
+    this.setPwdAlert(true);
+    this.pwdFlag = false;
+  }
+
+  this.checkValidForm();
+};
+
+SignUpModel.prototype.signUp = function () {
+  this.email = this.inputFieldForm[0].value;
+  this.pwd = this.inputFieldForm[1].value;
+
+  myRequest
+    .signUp(this.email, this.pwd)
+    .then((result) => {
+      /* TODO : should be implement after rest api is implemented */
+    })
+    .catch((err) => {
+      console.error(err);
+      this.setModal('에러발생', err.msg);
+      this.showModal();
+    });
+};
+
+export { SignInModel, SignUpModel };
