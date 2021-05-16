2682b8e81b346606559f55e06b590e6fb1a27091
hyeongjun94 <whow1101@naver.com>
2021-05-16 20:11:19 +0900

    Initialization for appsycn

---
+++ workout-stie/src/graphql/queries.js
@@ -0,0 +1,103 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getUser = /* GraphQL */ `
+  query GetUser($email: AWSEmail!) {
+    getUser(email: $email) {
+      email
+      nickname
+      gender
+      dailyInfos {
+        items {
+          id
+          date
+          weight
+          userEmail
+          createdAt
+          updatedAt
+        }
+        nextToken
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+
+export const listUsers = /* GraphQL */ `
+  query ListUsers(
+    $email: AWSEmail
+    $filter: ModelUserFilterInput
+    $limit: Int
+    $nextToken: String
+    $sortDirection: ModelSortDirection
+  ) {
+    listUsers(
+      email: $email
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+      sortDirection: $sortDirection
+    ) {
+      items {
+        email
+        nickname
+        gender
+        dailyInfos {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
+export const getDailyInfo = /* GraphQL */ `
+  query GetDailyInfo($id: ID!) {
+    getDailyInfo(id: $id) {
+      id
+      date
+      weight
+      userEmail
+      user {
+        email
+        nickname
+        gender
+        dailyInfos {
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const listDailyInfos = /* GraphQL */ `
+  query ListDailyInfos(
+    $filter: ModelDailyInfoFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listDailyInfos(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        date
+        weight
+        userEmail
+        user {
+          email
+          nickname
+          gender
+          createdAt
+          updatedAt
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
